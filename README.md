60e76d1a19eb73efcb88b1e1fce4d31c50d3e265
Mocanu Marian <viralnico@protonmail.com>
2023-04-02 11:11:31 +0000

    Working simple version

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tursib LiveMap
+
+### Tursib LiveMap is a web application that displays the real-time location of Tursib buses in Sibiu, Romania on a map. The app uses Leaflet to display the map and fetches the bus location data from the Tursib API. The app allows the user to follow a specific bus on the map and displays the routes of all available buses.
+
+# Installation
+
+### No installation is required to use the Tursib LiveMap application. Simply visit the webpage and the application will load in your web browser.
+
+### Usage
+
+- Visit the webpage in your web browser.
+- Select a bus route from the list of available routes.
+- Optionally, click the "Follow the Bus" checkbox to center the map view on the selected bus.
+- The map will display the real-time location of the selected bus.
+
+## Dependencies
+
+- Leaflet 1.7.1
+- Bulma 0.9.4
---
+++ js/functions.js
@@ -0,0 +1,139 @@
+// Define flags
+let stopRefresh = false;
+
+// Initialize the map with a default location and zoom level
+const map = L.map('map').setView([0, 0], 1);
+
+// Add a marker to the map at the given location
+const marker = L.marker([0, 0]).addTo(map);
+
+// Define the API endpoint URL
+// Use URL Path: ?bus=5&way=tour
+const urlParams = new URLSearchParams(window.location.search);
+const bus_number = urlParams.get('bus');
+const bus_direction = urlParams.get('way');
+const url = 'http://81.196.186.121:8080/TripPlanner/service/vehicles/line/' + bus_number + '/direction/' + bus_direction;
+
+// Add a tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+// Call fetchLines() to populate with all the Bus routes buttons
+fetchLines();
+
+try {
+     // Try to call setInitialView() to set the initial zoom level and center the map view on the initial point
+    setInitialView();
+
+    // Try to call autoRefresh()
+    // All the busses has an working interval
+    // In case of error (the bus is not working anymore) enable flag stopRefresh
+    autoRefresh();
+} catch (error) {
+    console.log(error);
+    stopRefresh = true;
+}
+
+// Use the fitBounds method to set the initial zoom level and center the map view on the initial point
+function setInitialView() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Get the latitude and longitude from the JSON data
+            const lat = data.vehiclesByLineAndDirection[0].lat / 1000000;
+            const lng = data.vehiclesByLineAndDirection[0].lng / 1000000;
+
+            // Set the initial view of the map
+            const bounds = [
+                [lat, lng]
+            ];
+            map.fitBounds(bounds, {
+                padding: [50, 50]
+            });
+        })
+        .catch(error => console.error(error));
+}
+
+// Define a function to update the map with the latest data
+function updateMap() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Get the latitude and longitude from the JSON data
+            if (data.vehiclesByLineAndDirection && data.vehiclesByLineAndDirection.length > 0) {
+                const lat = data.vehiclesByLineAndDirection[0].lat / 1000000;
+                const lng = data.vehiclesByLineAndDirection[0].lng / 1000000;
+
+                // Update the marker position
+                marker.setLatLng([lat, lng]);
+
+                // If the "Follow the Bus" checkbox is checked, update the map view to center on the marker
+                if (document.getElementById("followMarker").checked) {
+                    map.setView([lat, lng]);
+                }
+            } else {
+                console.log("GPS Autobuz offline")
+                stopRefresh = true
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            stopRefresh = true;
+        });
+}
+
+function autoRefresh() {
+    if (!stopRefresh) {
+        // Call the updateMap() function immediately to display the initial data
+        updateMap();
+        setTimeout(autoRefresh, 5000); // call again after 5 seconds
+    }
+}
+
+// Call API and fetch all the Bus Routes
+// Create two buttons for each ID
+function fetchLines() {
+    fetch('http://81.196.186.121:8080/TripPlanner/service/lines')
+        .then(response => response.json())
+        .then(data => {
+            const lines = data.allLines;
+            const container = document.createElement('div');
+            lines.forEach(line => {
+                const id = line.id;
+                if (id > 0) {
+                    const [desc1, desc2] = line.description.split(':');
+
+                    const lineElem = document.createElement('div');
+                    lineElem.classList.add('line');
+
+                    const idElem = document.createElement('span');
+                    idElem.innerText = `Autobuz: ${id}`;
+                    lineElem.appendChild(idElem);
+
+                    const first_route = document.createElement('button');
+                    first_route.innerText = desc1;
+                    first_route.classList.add('button', 'is-primary', 'is-full', 'is-responsive', 'wide-button');
+                    first_route.addEventListener('click', () => {
+                        window.location.href = `?bus=${id}&way=tour`;
+                    });
+                    lineElem.appendChild(first_route);
+
+                    const second_route = document.createElement('button');
+                    second_route.innerText = desc2;
+                    second_route.classList.add('button', 'is-primary', 'is-outlined', 'is-responsive', 'wide-button');
+                    second_route.addEventListener('click', () => {
+                        window.location.href = `?bus=${id}&way=retour`;
+                    });
+                    lineElem.appendChild(second_route);
+
+                    container.appendChild(lineElem);
+                }
+            });
+            document.body.appendChild(container);
+        })
+        .catch(error => console.error(error));
+}
